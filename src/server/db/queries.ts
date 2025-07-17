b01d1d1579d1a5a42ceb5a9924b832f56f67c39f
emmanuel macharia <samuelmarsha@outlook.com>
2025-07-17 10:31:00 +0300

    order by and fixes

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,13 @@
 import { db } from ".";
+import { z } from "zod";
 import { guest_feedback_table } from "./schema";
 import type { DB_GuestFeedback } from "./schema";
+
+const feedbackSchema = z.object({
+  name: z.string().trim().min(1).max(100),
+  email: z.string().trim().email(),
+  message: z.string().trim().min(1).max(1_000),
+});
 
 export const MUTATIONS = {
   createGuestFeedback: async function (input: {
@@ -8,13 +15,19 @@ export const MUTATIONS = {
     email: string;
     message: string;
   }) {
+    const parsedInput = feedbackSchema.safeParse(input);
+    if (!parsedInput.success) {
+      throw new Error(`Invalid input: ${parsedInput.error.message}`);
+    }
     try {
-      console.log("Creating guest feedback with input:", input);
-      const result = await db.insert(guest_feedback_table).values({
+      console.log("Creating guest feedback with input:", parsedInput.data);
+      const query = await db.insert(guest_feedback_table).values({
         name: input.name,
         message: input.message,
         email: input.email,
       });
+
+      const result: DB_GuestFeedback = query[0]!;
       console.log("Feedback created successfully:", result);
       return result;
     } catch (error) {

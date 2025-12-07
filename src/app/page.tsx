0dc4d8e067132574f8cb9ef87d3e8b4e292facc0
emmanuel macharia <samuelmarsha@outlook.com>
2025-12-07 18:16:22 +0300

    patch for CVE-2025-66478

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
         {/* Overlay for better text contrast */}
         <div className="absolute inset-0 -z-10 bg-black/40" />
         {/* Hero Content */}
-        <div className="relative z-10 flex w-full flex-col justify-end px-8 pb-16 lg:px-24">
+        <div className="relative z-10 flex w-full flex-col justify-end px-8 pb-16 lg:px-[25%]">
           <h1 className="mb-6 max-w-7xl font-sans text-5xl leading-none text-white md:text-6xl">
             Winnie Maingi
           </h1>
